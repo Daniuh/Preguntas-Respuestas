26386a122122c5cf43353bba6242546b493c5940
Daniel Naranjo <naranjobenavidesd@gmail.com>
2026-02-26 10:34:40 +0100

    Se hace re-factor de la función para validar las opciones

--- src/components/juego/verificarPregunta.js
+++ src/components/juego/verificarPregunta.js
@@ -1,67 +1,26 @@
 export function verificarRespuesta(respuesta) {
-    const opcion1  = document.querySelector('.divLi1');
-    const opcion2  = document.querySelector('.divLi2');
-    const opcion3  = document.querySelector('.divLi3');
-    const opcion4  = document.querySelector('.divLi4');
+    const opciones = document.querySelectorAll('.opcion');
 
-    listeners(opcion1, opcion2, opcion3, opcion4, respuesta);
+    listeners(opciones, respuesta);
 }
 
-function listeners(op1, op2, op3, op4, opCorrecta) {
-    let valor = 0;
-
+function listeners(opciones, opCorrecta) {
     console.log(opCorrecta);
 
-    validarOp1(op1, opCorrecta, valor);
-    validarOp2(op2, opCorrecta, valor);
-    validarOp3(op3, opCorrecta, valor);
-    validarOp4(op4, opCorrecta, valor);
+    validarOpcionSelec(opciones, opCorrecta);
 }
 
-function validarOp1(op1, respuesta, valorOp) {
-    op1.addEventListener('click', () => {
-        valorOp = 0;
+function validarOpcionSelec(opcion, respuesta) {
+    opcion.forEach(opcion => {
+        opcion.addEventListener('click', () => {
+            const indice = Number(opcion.dataset.indice);
 
-        if (valorOp === respuesta) {
-            console.log('Respuesta correcta');
-        } else {
-            console.log('Respuesta incorrecta');
-        }
+            if(indice === respuesta){
+                alert('Respuesta correcta');
+            } else {
+                alert('Respuesta incorrecta');
+            }
+        })
     });
 }
 
-function validarOp2(op2, respuesta, valorOp) {
-    op2.addEventListener('click', () => {
-        valorOp = 1;
-
-        if (valorOp === respuesta) {
-            console.log('Respuesta correcta');
-        } else {
-            console.log('Respuesta incorrecta');
-        }
-    });
-}
-
-function validarOp3(op3, respuesta, valorOp) {
-    op3.addEventListener('click', () => {
-        valorOp = 2;
-
-        if (valorOp === respuesta) {
-            console.log('Respuesta correcta');
-        } else {
-            console.log('Respuesta incorrecta');
-        }
-    });
-}
-
-function validarOp4(op4, respuesta, valorOp) {
-    op4.addEventListener('click', () => {
-        valorOp = 3;
-
-        if (valorOp === respuesta) {
-            console.log('Respuesta correcta');
-        } else {
-            console.log('Respuesta incorrecta');
-        }
-    });
-}
