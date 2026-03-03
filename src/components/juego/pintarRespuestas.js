05db8df556184cbb78d8af7cd7b41ab933d3feeb
Daniel Naranjo <naranjobenavidesd@gmail.com>
2026-03-03 18:53:32 +0100

    Se genera los puntos para el usuario por la respuesta correcta o incorrecta

--- src/components/juego/pintarRespuestas.js
+++ src/components/juego/pintarRespuestas.js
@@ -8,17 +8,32 @@ function pintarRespuestaCorrectaUsuario(resultEleccion, respuestaCorrecta, op) {
 
     if (resultEleccion) {
         const htmlRespuestaC = document.querySelector(`[data-indice="${respuestaCorrecta}"]`);
-        htmlRespuestaC.classList.remove('opcion');
+        const puntos         = document.querySelector('.pJuego');
 
+        htmlRespuestaC.classList.remove('opcion');
         htmlRespuestaC.classList.add('respuestaCorrecta');
+
+        let puntosActuales = Number(localStorage.getItem('puntajeUsuario'));
+        let puntosGanados  = 50;
+
+        const totalPuntos = puntosActuales + puntosGanados;
+
+        localStorage.setItem('puntajeUsuario', totalPuntos);
+
+        puntos.innerText = totalPuntos;
     } else {
         const htmlRespuestaC = document.querySelector(`[data-indice="${respuestaCorrecta}"]`);
         const htmlRespuestaI = document.querySelector(`[data-indice="${op}"]`);
+        const puntos         = document.querySelector('.pJuego');
 
         htmlRespuestaC.classList.remove('opcion');
         htmlRespuestaC.classList.add('respuestaCorrecta');
 
         htmlRespuestaI.classList.remove('opcion');
         htmlRespuestaI.classList.add('respuestaIncorrecta');
+
+        localStorage.setItem('puntajeUsuario', 0);
+        const puntosPerdidos = localStorage.getItem('puntajeUsuario');
+        puntos.innerText = puntosPerdidos;
     }
 }
