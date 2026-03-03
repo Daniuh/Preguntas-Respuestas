26d817b71405e9d19bbdee060ac12831286a9f96
Daniel Naranjo <naranjobenavidesd@gmail.com>
2026-03-03 18:35:35 +0100

    Se pinta la respuesta según la selección

--- src/components/juego/pintarRespuestas.js
+++ src/components/juego/pintarRespuestas.js
@@ -1,15 +1,24 @@
 export function pintarRespuesta(esCorrecta, opcion, respuesta) {
     console.log(esCorrecta, opcion, respuesta);
-    const opciones = document.querySelector('.opcion');
 
-    pintarRespuestaCorrectaUsuario(esCorrecta, respuesta, opciones);
+    pintarRespuestaCorrectaUsuario(esCorrecta, respuesta, opcion);
 }
 
-function pintarRespuestaCorrectaUsuario(resultEleccion, respuestaCorrecta, opciones) {
+function pintarRespuestaCorrectaUsuario(resultEleccion, respuestaCorrecta, op) {
+
     if (resultEleccion) {
-        
+        const htmlRespuestaC = document.querySelector(`[data-indice="${respuestaCorrecta}"]`);
+        htmlRespuestaC.classList.remove('opcion');
+
+        htmlRespuestaC.classList.add('respuestaCorrecta');
     } else {
+        const htmlRespuestaC = document.querySelector(`[data-indice="${respuestaCorrecta}"]`);
+        const htmlRespuestaI = document.querySelector(`[data-indice="${op}"]`);
 
+        htmlRespuestaC.classList.remove('opcion');
+        htmlRespuestaC.classList.add('respuestaCorrecta');
+
+        htmlRespuestaI.classList.remove('opcion');
+        htmlRespuestaI.classList.add('respuestaIncorrecta');
     }
-
 }
