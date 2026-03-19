da83f8be32b5fcf26a0bef2161604d6ee45ca6eb
Daniel Naranjo <naranjobenavidesd@gmail.com>
2026-03-19 17:23:26 +0100

    Se da funcionalidad a los botones

--- src/components/finalJuego/finalJuego.js
+++ src/components/finalJuego/finalJuego.js
@@ -1,3 +1,5 @@
+import { obtenerVistas } from '../../modules/cargarVistaA.js';
+
 export async function cargarFinalJuego() {
     const respuesta = await fetch('html/finalJuego.html');
     const finalJuego = await respuesta.text();
@@ -7,5 +9,23 @@ export async function cargarFinalJuego() {
 }
 
 function eventosFinalJuego() {
-    
+    const botonVolverJugar = document.querySelector('.buttonJugarFinal');
+    const botonRegresarInicio = document.querySelector('.buttonSalirFinal');
+
+
+    clickBtnJugar(botonVolverJugar);
+    clickBtnSalir(botonRegresarInicio);
 }
+
+function clickBtnJugar(btn) {
+    btn.addEventListener('click', () => {
+        obtenerVistas('tipo');
+    })
+}
+
+function clickBtnSalir(btn) {
+    btn.addEventListener('click', () => {
+        obtenerVistas('inicio');
+    })
+}
+
